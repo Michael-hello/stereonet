705b39d6eea7a4fee84f8cdaf904ca1163de15b0
Mike Jones <mjones@robertson-geo.com>
2025-03-03 11:39:31 +0000

    vite bug fix

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+
+// code taken from: https://stackoverflow.com/questions/69744253/vite-build-always-using-static-paths
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '', 
+})
